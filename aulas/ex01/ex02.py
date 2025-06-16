d3b5067b7e8d41f9078a34ef24c6408e674b2ed8
Isabella Pompeo <162395260+Isabella-Pompeo@users.noreply.github.com>
2025-06-16 19:53:57 -0300

    ajuste011

---
+++ aulas/ex01/ex02.py
@@ -0,0 +1,31 @@
+class nodo:
+    valor = 0
+    prox = nonc
+
+class LISTA_SIMPLES
+    pri = None
+    ult = None
+
+    def AddFinal(self, valor):
+        NovoNodo = Nodo()
+        NovoNodo.valor = valor
+
+        if self.pri is None and self.ult is None:
+            self.pri = NovoNodo
+            self.ult = NovoNodo
+        else:
+            self.ult.prox = NovoNodo
+            self.ult = NovoNodo
+
+    def print(self):
+        nA = self.pri
+
+        while nA is not None:
+            print(nA.valor, nA.prox)
+            nA = nA.prox
+
+    def AddInicio(self.valor):
+
+        else
+            NovoNodo.prox = self.pri
+            self.pri = NovoNodo
