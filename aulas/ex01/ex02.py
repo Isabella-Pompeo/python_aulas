4989b2320f30fd67df753f9869e3dc92dd972321
Isabella Pompeo <162395260+Isabella-Pompeo@users.noreply.github.com>
2025-08-04 19:35:09 -0300

    novoo

--- aulas/ex01/ex02.py
+++ aulas/ex01/ex02.py
@@ -1,43 +1,50 @@
-class nodo:
-    valor = 0
-    prox = nonc
+class Nodo:
+    def __init__(self, valor=0):
+        self.valor = valor
+        self.prox = None
 
-class LISTA_SIMPLES
-    pri = None
-    ult = None
+class ListaSimples:
+    def __init__(self):
+        self.pri = None
+        self.ult = None
 
     def AddFinal(self, valor):
-        NovoNodo = Nodo()
-        NovoNodo.valor = valor
+        novo_nodo = Nodo(valor)
 
-        if self.pri is None and self.ult is None:
-            self.pri = NovoNodo
-            self.ult = NovoNodo
+        if self.pri is None:
+            self.pri = novo_nodo
+            self.ult = novo_nodo
         else:
-            self.ult.prox = NovoNodo
-            self.ult = NovoNodo
+            self.ult.prox = novo_nodo
+            self.ult = novo_nodo
 
-    def print(self):
+    def AddInicio(self, valor):
+        novo_nodo = Nodo(valor)
+
+        if self.pri is None:
+            self.pri = novo_nodo
+            self.ult = novo_nodo
+        else:
+            novo_nodo.prox = self.pri
+            self.pri = novo_nodo
+
+    def AddApos(self, novo_valor, valor_busca):
+        novo_nodo = Nodo(novo_valor)
         nA = self.pri
 
         while nA is not None:
-            print(nA.valor, nA.prox)
+            if nA.valor == valor_busca:
+                novo_nodo.prox = nA.prox
+                nA.prox = novo_nodo
+                if novo_nodo.prox is None:
+                    self.ult = novo_nodo
+                return
             nA = nA.prox
 
-    def AddInicio(self.valor):
+        print("Valor não encontrado!")
 
-        else
-            NovoNodo.prox = self.pri
-            self.pri = NovoNodo
-
-    def AddApos(self, NovoValor, ValorBusca)
-
-        else:
-            nA = self.pri
-             
-            while nA.prox is not self.ult:
-                if nA.valor == valor.busca:
-                    NovoNodo.prox = nA.prox
-                    nA.paox = NovoNodo
-                nA = nA.prox
-            self.Add
+    def imprimir(self):
+        nA = self.pri
+        while nA is not None:
+            print(nA.valor)
+            nA = nA.prox
