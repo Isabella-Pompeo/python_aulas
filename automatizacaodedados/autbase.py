ccb56f060037d1729097ed91e2710cde202ae74c
Isabella Pompeo <162395260+Isabella-Pompeo@users.noreply.github.com>
2025-08-18 23:40:44 -0300

    ok

--- automatizacaodedados/autbase.py
+++ automatizacaodedados/autbase.py
@@ -1,3 +1,13 @@
 import pandas as pd
 
 #importar base de dados
+
+#visualizar base de dados
+
+#faturamento por loja
+
+#quantidade de vendidos por loja
+
+#ticked medio por pro em cada loja
+
+#enviar um email com
